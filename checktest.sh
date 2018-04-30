be1df84d1495196151ad22f1ab4348fdd37c4f3e
Sandeep Kumar <sandeep007734@gmail.com>
2018-04-30 07:23:57 +0530

    working

--- checktest.sh
+++ checktest.sh
@@ -5,5 +5,6 @@ opt -load /home/sandeep/Downloads/llvm/build/lib/LLVMHello.so -ModulePassUtil -S
 opt -load /home/sandeep/Downloads/llvm/build/lib/LLVMHello.so -ModulePassUtil -S partial_test.ll > partial_test.ir 2> /dev/null
 
 diff working_test.ir test.ir
+echo "One done"
 diff working_partial_test.ir partial_test.ir
 #vimdiff test.ll test.ir
