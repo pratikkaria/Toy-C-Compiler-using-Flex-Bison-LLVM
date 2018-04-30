a6144c1fe8552f47357ecd045a615e3bc2fda5ea
Sandeep Kumar <sandeep007734@gmail.com>
2018-04-30 07:15:28 +0530

    partial working

--- checktest.sh
+++ checktest.sh
@@ -1,3 +1,9 @@
+./getllvmircode.sh test.c test.ll > /dev/null
+./getllvmircode.sh partial_test.c partial_test.ll > /dev/null
+
 opt -load /home/sandeep/Downloads/llvm/build/lib/LLVMHello.so -ModulePassUtil -S test.ll > test.ir 2> /dev/null
+opt -load /home/sandeep/Downloads/llvm/build/lib/LLVMHello.so -ModulePassUtil -S partial_test.ll > partial_test.ir 2> /dev/null
+
 diff working_test.ir test.ir
+diff working_partial_test.ir partial_test.ir
 #vimdiff test.ll test.ir
---
+++ partial_test.c
@@ -0,0 +1,17 @@
+extern int printf(char const *format, ...);
+
+
+int main(){
+
+    int la = 10;
+    int lb = 20;
+    int lc = la+lb*2;
+    int ld = la+lb*2;
+
+    printf("%d\n", lc);
+    printf("%d\n", ld);
+
+
+    
+    return 0;
+}
